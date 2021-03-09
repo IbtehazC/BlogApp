b361c76bd48a69c621bc76216ca1e627b24e91e1
IbtehazC <ibtehaz.chowdhury@gmail.com>
2021-03-09 17:30:06 +0600

    Followers Bug FIx

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class FollowControllers : BaseApiController
+    public class FollowController : BaseApiController
     {
         [HttpPost("{username}")]
         public async Task<IActionResult> Follow(string username)
